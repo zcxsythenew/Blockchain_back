d562b16f01e6ce3747e6af4aa3ca53f6a0a3f7a8
zcxsythenew <wugzh3@mail2.sysu.edu.cn>
2019-12-11 17:01:58 +0800

    changed ConnectConstants

--- src/main/java/com/example/demo/constants/ConnectConstants.java
+++ src/main/java/com/example/demo/constants/ConnectConstants.java
@@ -1,7 +1,7 @@
 package com.example.demo.constants;
 
 public class ConnectConstants {
-    public static final Integer CONNECT_SECONDS = 300;
-    public static final Integer CONNECT_SLEEP_PER_MILLIS = 100;
-    public static final Integer TIME_OUT = 300000;
+    public static final Integer CONNECT_SECONDS = 30;
+    public static final Integer CONNECT_SLEEP_PER_MILLIS = 1;
+    public static final Integer TIME_OUT = 30000;
 }
